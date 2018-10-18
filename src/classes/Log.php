74f2dc5f02cbf657cda23228377fdc9046795993
Nick Kravchuk <nick@hexbrain.com>
2018-10-18 10:49:31 +0300

    Added description to function write for Log class

--- src/classes/Log.php
+++ src/classes/Log.php
@@ -3,7 +3,11 @@
 class Log
 {
     const LOG_FILE = 'var/error.log';
-
+    
+    /**
+	 * Write error to file
+     * @param $error
+     */
     public function write($error)
     {
         error_log($error, 3, $_SERVER['DOCUMENT_ROOT'] . self::LOG_FILE);
